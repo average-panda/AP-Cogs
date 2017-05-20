97d87049e5e4d9b8fce062c89cc5d9d2c60bf265
average-panda <edy.tlmch@gmail.com>
2017-05-20 10:34:19 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,6 +6,11 @@ class test:
 
     def __init__(self, bot):
         self.bot = bot
+        
+    testEmbed = discord.Embed(title='test', type='rich', description='test', url='https://github.com/Rapptz/discord.py/issues/424', colour = discord.Colour.blue())
+    testEmbed.set_thumbnail(url='https://github.com/Rapptz/discord.py/issues/424')
+    testEmbed.set_author(name='test')
+    testEmbed.set_footer(text='test')
 
     @commands.command()
     async def punch(self, user : discord.Member):
@@ -20,6 +25,13 @@ class test:
 
         #Your code will go here
         await self.bot.say("I can do stuff!")
+        
+    @commands.command()
+    async def embed(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say(message.channel, embed=testEmbed)
 
 def setup(bot):
     bot.add_cog(test(bot))
