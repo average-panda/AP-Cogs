d288541904c587cb276d0de2b17700490db93d5b
average-panda <edy.tlmch@gmail.com>
2017-05-20 13:44:37 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,17 +8,17 @@ class test:
     def __init__(self, bot):
         self.bot = bot
         
-    embed = discord.Embed(title="title ~~(did you know you can have markdown here too?)~~", colour=discord.Colour(0x687508), url="https://discordapp.com", description="this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```")
+    Tembed = discord.Embed(title="title ~~(did you know you can have markdown here too?)~~", colour=discord.Colour(0x687508), url="https://discordapp.com", description="this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```")
     
-    embed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
-    embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
-    embed.set_author(name="author name", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
-    embed.set_footer(text="footer text", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+    Tembed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
+    Tembed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
+    Tembed.set_author(name="author name", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+    Tembed.set_footer(text="footer text", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
 
-    embed.add_field(name="1", value="some of these properties have certain limits...")
-    embed.add_field(name="2", value="try exceeding some of them!")
-    embed.add_field(name="3", value="an informative error should show up, and this view will remain as-is until all issues are fixed")
-    embed.add_field(name="<:thonkang:219069250692841473>", value="???")
+    Tembed.add_field(name="1", value="some of these properties have certain limits...")
+    Tembed.add_field(name="2", value="try exceeding some of them!")
+    Tembed.add_field(name="3", value="an informative error should show up, and this view will remain as-is until all issues are fixed")
+    Tembed.add_field(name="<:thonkang:219069250692841473>", value="???")
 
     @commands.command()
     async def punch(self, user : discord.Member):
@@ -39,14 +39,14 @@ class test:
         """This does stuff!"""
 
         #Your code will go here
-        await self.bot.say(embed=testEmbed)
+        await self.bot.say(embed=Tembed)
         
     @commands.command()
     async def embed2(self):
         """This does stuff!"""
         
         #Your code will go here
-        await self.bot.say(content="this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", embed=embed)
+        await self.bot.say(content="this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", embed=Tembed)
 
 def setup(bot):
     bot.add_cog(test(bot))
