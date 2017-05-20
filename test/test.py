75826eb9ae679ff8b914fe16253817c3df5fd7e9
average-panda <edy.tlmch@gmail.com>
2017-05-20 09:38:00 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,11 +8,11 @@ class test:
         self.bot = bot
 
     @commands.command()
-    async def mycom(self):
-        """This does stuff!"""
+async def punch(self, user : discord.Member):
+    """I will puch anyone! >.<"""
 
-        #Your code will go here
-        await self.bot.say("I can do stuff!")
+    #Your code will go here
+    await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
 
 def setup(bot):
     bot.add_cog(test(bot))
